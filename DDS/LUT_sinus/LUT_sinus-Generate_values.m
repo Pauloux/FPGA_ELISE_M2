ab4cb1ab25c3f356fa16871c7f960b0b501ba383
Pauloux <paulouxps3@gmail.com>
2026-09-15 20:46:58 +0200

    Renaming of folders and variables

---
+++ DDS/LUT_sinus/LUT_sinus-Generate_values.m
@@ -0,0 +1,12 @@
+phase_N = 10;			% phase bits -> 1024 points
+amplitude_N = 14;		% amplitude bits
+nb_points = 2^phase_N;		% 1024
+max_val = 2^amplitude_N - 1;	% 16383
+
+t = 0:nb_points-1;
+
+table = round( (sin(2*pi*t/nb_points) + 1) /2 * max_val );
+
+for k = 1:nb_points
+  printf("\t\t%d => \"%s\",\n", k-1, dec2bin(table(k), amplitude_N));
+end
