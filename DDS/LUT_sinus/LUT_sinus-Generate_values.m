6d3ca8df5c342ad53f41bc17d8ca29c593e1b876
mbellaud26 <maxime.bellaud@gmail.com>
2026-09-23 11:25:46 +0200

    LUT Sinus: LUT is dependent of N and we don't need to regenerate LUT

--- DDS/LUT_sinus/LUT_sinus-Generate_values.m
+++ DDS/LUT_sinus/LUT_sinus-Generate_values.m
@@ -1,4 +1,4 @@
-phase_N = 10;			% phase bits -> 1024 points
+phase_N = 16;			% phase bits -> 1024 points
 amplitude_N = 14;		% amplitude bits
 nb_points = 2^phase_N;		% 1024
 max_val = 2^amplitude_N - 1;	% 16383
@@ -8,5 +8,5 @@ t = 0:nb_points-1;
 table = round( (sin(2*pi*t/nb_points) + 1) /2 * max_val );
 
 for k = 1:nb_points
-  printf("\t\t%d => \"%s\",\n", k-1, dec2bin(table(k), amplitude_N));
+  printf("\t\t%d \t=> \t\"%s\",\n", k-1, dec2bin(table(k), amplitude_N));
 end
